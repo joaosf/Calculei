a5063a167dbc83680c3cb52a8f59b5dd44ca9da6
DBServer - Gabriel Novakovski Nunes <t.nunes@ADMM001108.local>
2019-04-24 18:03:22 -0300

    adicionando gitignore

---
+++ app/src/main/java/com/joao/calculei/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.joao.calculei.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.joao.calculei.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
